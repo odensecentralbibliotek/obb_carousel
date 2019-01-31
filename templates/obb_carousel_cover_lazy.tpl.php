4b1a8843aa793e4f0160c1ea7145779ecb927304
memleakz <dev@dev-VirtualBox>
2019-01-31 09:59:20 +0100

    improvements

--- templates/obb_carousel_cover_lazy.tpl.php
+++ templates/obb_carousel_cover_lazy.tpl.php
@@ -1,5 +1,8 @@
 <?php
-echo  '<div class="' . implode(' ', $variables['lazy_classes']) . '">' . $variables['lazy_image'] . '</div>';
+if(isset($variables['lazy_image']))
+{
+    echo  '<div class="' . implode(' ', $variables['lazy_classes']) . '">' . $variables['lazy_image'] . '</div>';
+}
 ?>
 
 
